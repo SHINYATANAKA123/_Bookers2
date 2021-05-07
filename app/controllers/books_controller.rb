7ac3d6ac7b2c0aa95517b28efc1c9aae02f56aa0
SHINYATANAKA123 <123asde.med@gmail.com>
2021-05-07 15:52:47 +0000

    [Update] 投稿機能のテンプレート化(仮)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.new
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -11,15 +15,15 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to '/top'
+    redirect_to book_path(@book)
   end
 
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :opinion)
   end
